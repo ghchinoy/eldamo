3d19d8ff71bc18a5a20c386d7bc8c0899fdac748
Paul Strack <pfstrack@gmail.com>
2026-03-28 16:51:41 -0700

    v0.8.12.2

--- src/test/java/xdb/util/ProbTest.java
+++ src/test/java/xdb/util/ProbTest.java
@@ -7,6 +7,58 @@ import org.junit.Test;
 
 public class ProbTest {
 
+    @Test
+    public void testSkillMovesD20() {
+        double[] poor = new double[14];
+        double[] mixed = new double[14];
+        double[] good = new double[14];
+        double[] total = new double[14];
+        for (int bonus = -1; bonus <= 12; bonus ++) {
+            for (int d1 = 1; d1 <= 20; d1++) {
+                for (int d2 = 1; d2 <= 20; d2++) {
+                    total[bonus+1]++;
+                    int die = d1; // < d2 ? d1 : d2;
+                    int result = die + bonus;
+                    if (die == 1 || result < 11) poor[bonus+1]++;
+                    else if (result < 16) mixed[bonus+1]++;
+                    else good[bonus+1]++;
+                }
+            }
+        }
+        for (int bonus = -1; bonus <= 12; bonus ++) {
+            System.out.println(poor[bonus+1]/total[bonus+1]
+                + "\t" + mixed[bonus+1]/total[bonus+1]
+                + "\t" + good[bonus+1]/total[bonus+1]);
+        }
+    }
+
+    @Test
+    public void testSkillMoves2D10() {
+        double[] poor = new double[14];
+        double[] mixed = new double[14];
+        double[] good = new double[14];
+        double[] total = new double[14];
+        for (int bonus = -1; bonus <= 12; bonus ++) {
+            for (int d1 = 1; d1 <= 10; d1++) {
+                for (int d2 = 1; d2 <= 10; d2++) {
+                    for (int d3 = 1; d3 <= 10; d3++) {
+                        total[bonus+1]++;
+                        int die = d1 + d2; // + d3 - min(d1, d2, d3);
+                        int result = die + bonus;
+                        if (die == 2 || result < 12) poor[bonus+1]++;
+                        else if (result < 17) mixed[bonus+1]++;
+                        else good[bonus+1]++;
+                    }
+                }
+            }
+        }
+        for (int bonus = -1; bonus <= 12; bonus ++) {
+            System.out.println(poor[bonus+1]/total[bonus+1]
+                + "\t" + mixed[bonus+1]/total[bonus+1]
+                + "\t" + good[bonus+1]/total[bonus+1]);
+        }
+    }
+
 	@Test
 	public void test() {
 		for (int bonus = 0; bonus <= 5; bonus++) {
@@ -123,4 +175,24 @@ public class ProbTest {
 	public String perc(double value) {
 		return new BigDecimal(value * 100).setScale(2, RoundingMode.HALF_UP).toString();
 	}
+
+    public int max(int... values) {
+        int max = 0;
+        for (int value : values) {
+            if (value > max) {
+                max = value;
+            }
+        }
+        return max;
+    }
+
+    public int min(int... values) {
+        int min = 1000;
+        for (int value : values) {
+            if (value < min) {
+                min = value;
+            }
+        }
+        return min;
+    }
 }
